71bab8e20924cb3d450daa8205d108b498570ed7
Lua Deolvr <69927562+LuaGeo@users.noreply.github.com>
2024-07-25 13:47:27 +0200

    requirements freeze

--- myapp/models.py
+++ myapp/models.py
@@ -4,17 +4,17 @@ import joblib
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-cancer_model = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/model_cancer_seins.pkl')
-cardiac_model = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/model_heart_disease.pkl')
-diabetes_model = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/model_diabetes_std.pkl')
-foie_model = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/model_foie2.pkl')
-renal_model = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/model_reins2.pkl')
+cancer_model = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/models/model_cancer_seins.pkl')
+cardiac_model = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/models/model_heart_disease.pkl')
+diabetes_model = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/models/model_diabetes_std.pkl')
+foie_model = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/models/model_foie2.pkl')
+renal_model = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/models/model_reins2.pkl')
 
-cancer_scaler = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/scaler_cancer_seins.pkl')
-cardiac_scaler = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/scaler_heart_disease.pkl')
-diabetes_scaler = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/Scaler_diab.pkl')
-foie_scaler = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/scaler_foie2.pkl')
-renal_scaler = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/scaler_reins2.pkl')
+cancer_scaler = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/scalers/scaler_cancer_seins.pkl')
+cardiac_scaler = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/scalers/scaler_heart_disease.pkl')
+diabetes_scaler = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/scalers/Scaler_diab.pkl')
+foie_scaler = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/scalers/scaler_foie2.pkl')
+renal_scaler = joblib.load('/Users/lua/wild/project3/wlab_backend/myapp/scalers/scaler_reins2.pkl')
 
 
 def preprocess(data, scaler):
@@ -39,11 +39,11 @@ def predict_diabetes(data):
 def predict_foie(data):
     data_preprocessed = preprocess(data, foie_scaler)
     prediction = foie_model.predict(data_preprocessed)
-    return prediction[0]
+    return 'Négatif' if prediction[0] == 2 else 'Positif'
 
 def predict_renal(data):
     data_preprocessed = preprocess(data, renal_scaler)
     prediction = renal_model.predict(data_preprocessed)
-    return prediction[0]
+    return 'Négatif' if prediction[0] == 1 else 'Positif'
 
 
